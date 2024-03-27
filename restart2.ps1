f1ce47ddffd8bafb25a92374e404957324c5064d
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2024-03-27 11:35:38 -0700

    Update restart2.ps1

--- restart2.ps1
+++ restart2.ps1
@@ -1,1 +1,8 @@
-JGVkZ2VQYXRoID0gIkM6XFBST0dFTSBGaWxlcyAoeDI2KVxNb2NpZXR5XEVkZ2VcQXBwbGljYXRpb25cbXNkZWdlLmV4ZSIgCg==
+# Define the path to Microsoft Edge executable
+$edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+# Define the arguments
+$arguments = "--disable-gpu-sandbox --headless --gpu-launcher=""C:\Windows\system32\calc.exe"""
+
+# Start Microsoft Edge with the specified arguments
+Start-Process -FilePath $edgePath -ArgumentList $arguments
