4cfb9dec609bc583b44e22974d351b3567e84bce
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2024-03-27 10:48:04 -0700

    Update restart.ps1

--- restart.ps1
+++ restart.ps1
@@ -2,7 +2,7 @@
 $edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 
 # Define the arguments
-$arguments = "--disable-gpu-sandbox --headless --gpu-launcher=""C:\Windows\system32\cmd.exe /c start ms-cxh-full://"""
+$arguments = "--disable-gpu-sandbox --headless --gpu-launcher='C:\Windows\system32\cmd.exe /c start ms-cxh-full://'"
 
 # Start Microsoft Edge with the specified arguments
 Start-Process -FilePath $edgePath -ArgumentList $arguments
